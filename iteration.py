c9dffd41edb4e2e918597f59aa64af0f68789ea9
marisleysisdelacruz16 <marisleidy06@gmail.com>
2019-09-08 16:47:16 -0400

    Initial Commit

--- iteration.py
+++ iteration.py
@@ -2,48 +2,7 @@
 Lab 1: the power function
 cs105 Introduction to Computer Science
 Haverford College
-<replace with your name>
-Compute base to the exp power, for integer exp>=0,
-using a straightforward translation of the rules
-     base**0 = 1
-     base**exp = base * base* base* ... exp times
-There are much more efficient algorithms for exponentiation,
-but our emphasis here is on simplicity rather than speed.
-Note that we do not allow negative or zero exponents,
-  as our current algorithm does not handle them
-Examples of the power function:
->>> power_using_iteration(5, 0)
-1
-
->>> power_using_iteration(3, 3)
-27
-
->>> power_using_iteration(3.0, 3)  # note that the ".0" is retained in the answer
-27.0
-
->>> power_using_iteration(1.5, 2)   # 3/2 squared is 9/4
-2.25
-
-When we don't want to write out _all_ the digits of the answer,
-we put the "# doctest: +ELLIPSIS" at the end of the example question, like so
->>> power_using_iteration(0.9, 5)  # doctest: +ELLIPSIS
-0.5904900...
-
-Testing for exceptions, read the following for further explaination on how to doctest exception
-# https://docs.python.org/2.4/lib/doctest-exceptions.html
->>> power_using_iteration("2", 5)
-Traceback (most recent call last):
-TypeError: Base should be either integer or float
-
->>> power_using_iteration(2, 0.5)
-Traceback (most recent call last):
-TypeError: Exponent is supposed to be integer
-
->>> power_using_iteration(2, -5)
-Traceback (most recent call last):
-ValueError: Exponent should be positive integer or zero
-
-Ignore warnings if you see any
+Marisleysis De La Cruz
 """
 def power_using_iteration(base, exp):
     """postcondition: the result is the base raised to the exp power,
@@ -55,25 +14,33 @@ def power_using_iteration(base, exp):
     if not isinstance(exp, int):
         raise TypeError("Exponent is supposed to be integer")
 
-    if isinstance(exp, int) and not exp>=0:
-        raise ValueError("Exponent should be positive integer or zero")
-
     result=1
-    if exp == 0:
-        return result
-    else:
-        while exp:
+    for power_using_iteration in range (abs(exp)):
+        if exp == 0:
+            return result
+        if exp < 0:
+            result *= 1.0/base
+        else:
+         while exp:
             result= result*base
             exp = exp-1
     return result
 
-def simple_power_ui():
-    # doctest use, as per http://docs.python.org/lib/module-doctest.html
-    import doctest
-    print("Trying out tests given at the beginning of this script")
-    doctest.testmod()
 
-# The execution starts here
-# I copied this from http://docs.python.org/lib/module-doctest.html
-if __name__ == "__main__":
-    simple_power_ui()
+# Test Cases
+'''
+>>> power_using_iteration(1.0, -2)
+1.0
+
+>>> power_using_iteration(1.2, -3) #doctest: +ELLIPSIS
+0.578703...
+
+>>> power_using_iteration(5, -1) 
+0.2
+
+>>> power_using_iteration(10, -4)  
+0.000100...
+
+>>> power_using_iteration(0.6, -5)   
+12.860082...
+'''
